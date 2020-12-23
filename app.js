5b4928640aca54b884a5af9461cfac6aeceface2
unknown <csiamunyanga@hotmail.com>
2020-12-23 15:56:28 +0200

    Remove unnecassary dependancies
    - This is to be done because the template code adopted for the server
      contained dependencies that are irrelevant to this project
    - This is also to reduce the size or the project

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require( 'dotenv/config' );
 const express = require( 'express' );
 const app = express();
-const mongoose = require( 'mongoose' );
 const bodyParser = require( 'body-parser' );
 const morgan_log = require( 'morgan' );
 
